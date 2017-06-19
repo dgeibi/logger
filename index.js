37e6ab493fd797c519a22d84dca67fd2a85fa1ea
dgeibi <png.inside@gmail.com>
2017-06-19 15:36:08 +0800

    use chalk

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const colors = require('colors');
-const { EOL } = require('os');
+const chalk = require('chalk');
 const stringify = require('json-stringify-safe');
 
-colors.setTheme({
-  silly: 'rainbow',
-  input: 'grey',
+const colors = {
   verbose: 'cyan',
-  prompt: 'grey',
   info: 'green',
-  data: 'grey',
-  help: 'cyan',
   warn: 'yellow',
   debug: 'blue',
   error: 'red',
-});
+};
+
+const aliases = {
+  err: 'error',
+};
 
 function log(color, ...args) {
   const str = args.map((arg) => {
@@ -24,21 +22,19 @@ function log(color, ...args) {
     }
     return arg;
   }).join(' ');
-  console.log(colors[color](str));
+  console.log(chalk[color](str));
 }
 
-function bind(fn, ...args) {
-  return fn.bind(null, ...args);
-}
+const logger = {
+  log: console.log,
+};
 
-const logger = {};
+Object.keys(colors).forEach((x) => {
+  logger[x] = log.bind(null, colors[x]);
+});
 
-logger.log = (...args) => console.log(...args);
-logger.warn = bind(log, 'warn');
-logger.error = bind(log, 'error');
-logger.err = bind(log, 'error');
-logger.info = bind(log, 'info');
-logger.debug = bind(log, 'debug');
-logger.verbose = bind(log, 'verbose');
+Object.keys(aliases).forEach((x) => {
+  logger[x] = logger[aliases[x]];
+});
 
 module.exports = logger;
